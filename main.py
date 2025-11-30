dbf1318742e221b641ee0ed21214502b608a3dbe
zoforo <47060165+zoforo@users.noreply.github.com>
2025-11-30 00:31:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -184,24 +184,18 @@ with tab3:
         
     # --- GRÁFICO 6: BAR PLOT (Pedidos por Zona Restaurante) ---
     with col_z2:
-        import streamlit as st
-        import pandas as pd
-        import matplotlib.pyplot as plt
-        
-        st.title("Restaurantes más populares por zonas")
-        df = pd.read_csv('Delivery.csv')  #Carga datos del df
-        st.sidebar.header("Opciones de Diseño")
-        color_elegido = st.sidebar.color_picker("Elige un color para las barras", "#00f900") #Permite al usuario elegir colores
-        datos_grafico = df["restaurant_zone"].value_counts() #Ordena de mayor a menor
+st.sidebar.header("Opciones de Diseño")
+color_elegido = st.sidebar.color_picker("Elige un color para las barras", "#00f900") #Permite al usuario elegir colores
+datos_grafico = df["restaurant_zone"].value_counts() #Ordena de mayor a menor
 
-       
-        fig6, ax6 = plt.subplots(figsize=(6, 4))
-        ax.bar(datos_grafico.index, datos_grafico.values, color=color_elegido) 
-        ax.set_title("Cantidad de Pedidos por Zona")
-        ax.set_xlabel("Zona del Restaurante")
-        ax.set_ylabel("Total de Pedidos")
-        st.pyplot(fig6, use_container_width=False)
 
-        
-        st.write("Detalle de los datos:", datos_grafico) #Números exactos
+fig6, ax6 = plt.subplots(figsize=(6, 4))
+ax.bar(datos_grafico.index, datos_grafico.values, color=color_elegido) 
+ax.set_title("Cantidad de Pedidos por Zona")
+ax.set_xlabel("Zona del Restaurante")
+ax.set_ylabel("Total de Pedidos")
+st.pyplot(fig6, use_container_width=False)
+
+
+st.write("Detalle de los datos:", datos_grafico) #Números exactos
                 
